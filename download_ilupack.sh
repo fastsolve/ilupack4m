3bd700f69f5283e0c513ba064aa131dc68eb04e4
Xiangmin Jiao <xmjiao@gmail.com>
2017-04-24 14:29:23 -0400

    Moved download script into makefile.

--- download_ilupack.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Download the precompiled libraries of ilupack v2.4 for GNU64
-# It must be executed in this directory
-
-curl -L http://www.icm.tu-bs.de/~bolle/ilupack/download/ilupack05102016.zip | \
-        bsdtar zxf - --strip-components 1 ilupack/lib/GNU64 &&
-        rm -f lib/GNU64/libilupack_mc64.a
