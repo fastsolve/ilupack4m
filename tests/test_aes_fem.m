064a203cd8dd60ac1c7b2a3395fd943ee9b09819
Xiangmin Jiao <xmjiao@gmail.com>
2017-06-25 16:22:38 -0400

    Removed degree-6

--- tests/test_aes_fem.m
+++ tests/test_aes_fem.m
@@ -1,6 +1,6 @@
 function test_aes_fem
 
-[~, output] = system('gd-ls -p 0ByTwsK5_Tl_PdDVSWUZzUmxpMWs "*"');
+[~, output] = system('gd-ls -p 0ByTwsK5_Tl_PdDVSWUZzUmxpMWs "Neumann_*_d=[24].mat"');
 
 files = strsplit(output, '\n');
 rtol = 1.e-5;
@@ -16,7 +16,7 @@ for i=1:length(files)
             s.aes_fe3_linsys.col_ind, s.aes_fe3_linsys.val);
         b = s.aes_fe3_linsys.b;
         x = gmresMILU(A, b, [], rtol);
-        fprintf(1, 'Relative residual is %1.f, with tolerance %1.f\n', ...
+        fprintf(1, 'Relative residual is %1.g, with tolerance %1.g\n', ...
             norm(A * x - b) / norm(b), rtol);
     end
 end
