691c2566893a4868042fd2d085e7a62353ae2d29
Xiangmin Jiao <xmjiao@gmail.com>
2017-06-25 17:26:09 -0400

    Added help message

--- load_milu.m
+++ load_milu.m
@@ -1,6 +1,6 @@
 function load_milu(varargin)
 
-% ilupack4m depends on paracoder for testing. Need to load it first
+% ilupack4m depends on paracoder for testing. Load it first
 if ~exist('load_m2c.m', 'file')
     if exist('../paracoder/load_m2c.m', 'file')
         run('../paracoder/load_m2c.m')
@@ -9,9 +9,14 @@ elseif ~exist('m2c.m', 'file')
     load_m2c
 end
 
-% Load the Petsc4m module
+% Load ILUPACK
 addpath(miluroot); %#ok<*MCAP>
 addpath([miluroot '/matlab/ilupack']);
 addpath([miluroot '/util']);
+
+% Show help message
+if ~exist(['ILUfactor.' mexext], 'file')
+    disp('Please run build_milu to build ILUPACK.');
+end
 
 end
