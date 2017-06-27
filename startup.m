d36c8d65d094492687a4b95db575b069fbc2822d
Xiangmin Jiao <xmjiao@gmail.com>
2017-06-27 01:15:26 -0400

    Updated startup script

--- startup.m
+++ startup.m
@@ -1,3 +1,10 @@
-% This script is called automatically when MATLAB and Octave exists.
+% Startup script for petsc4m
+
+if exist('OCTAVE_VERSION', 'builtin')
+    more off;
+else
+    warning('off', 'MATLAB:mex:GccVersion_link');
+end
+load_petsc;
 
 load_milu;
