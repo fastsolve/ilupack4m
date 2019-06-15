9b457f52ee4055894c8f1518e899a1bb728033c6
Xiangmin Jiao <xmjiao@gmail.com>
2019-06-15 00:00:20 -0400

    changed to use mc64 with Octave

--- util/build_milu.m
+++ util/build_milu.m
@@ -2,13 +2,13 @@ function build_milu(varargin)
 % Build ILUPACK
 
 if ~exist('OCTAVE_VERSION', 'builtin') || nargin >= 1 && isequal(varargin{1}, '-matlab')
-    fprintf(1, 'Building for MATLAB...\n')
-    system(['cd ', miluroot, '/makefiles; make TARGET=MATLAB']);
+    fprintf(1, 'Building for MATLAB with MUMPS...\n')
+    system(['cd ', miluroot, '/makefiles; make -f makefile TARGET=MATLAB']);
 
     LIBDIR = [miluroot, '/lib/GNU64_long'];
 else
-    fprintf(1, 'Building for Octave...\n')
-    system(['cd ', miluroot, '/makefiles; make TARGET=Octave']);
+    fprintf(1, 'Building for Octave with MC64...\n')
+    system(['cd ', miluroot, '/makefiles; make -f makefile_mc64 TARGET=Octave']);
     LIBDIR = [miluroot, '/lib/GNU64'];
 end
 
